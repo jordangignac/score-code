d234feb093c4ded2c1be549ef5b00bde93287521
Jordan Gignac <gignjo16@gmail.com>
2021-09-05 18:49:23 -0400

    Update base level files with added deps and prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('daisyui')],
+  variants: {extend: {}},
+  theme: {extend: {}},
+};
