3f0039ccc3cd5439e4a9536e36709abc28e210a9
Jordan Gignac <gignjo16@gmail.com>
2021-09-05 19:10:49 -0400

    Add initial wip page index

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,55 @@
-import Head from 'next/head'
+import React from 'react';
+import axios from 'axios';
+import Head from 'next/head';
+
+import Table from '../components/table';
+import Search from '../components/search';
+import Header from '../components/header';
+import Pagination from '../components/pagination';
+
+import headers from '../data/headers.json';
 
 export default function Home() {
+  const [rows, setRows] = React.useState([]);
+  const [sortField, setSortField] = React.useState('');
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const [sortDirection, setSortDirection] = React.useState('asc');
+
+  React.useEffect(() => {
+    axios
+      .get('/api/data', {
+        params: {
+          field: sortField,
+          search: searchTerm,
+          direction: sortDirection,
+        },
+      })
+      .then(({data}) => setRows(data))
+      .catch(err => console.error('Error', err));
+  }, [sortField, searchTerm, sortDirection]);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+    <React.Fragment data-theme="light">
+      <div className="w-full p-5">
+        <Head>
+          <title>score-code</title>
+          <meta name="description" content="nfl-rushing code challenge" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <Search
+          {...{
+            searchTerm,
+            setSearchTerm,
+            sortField,
+            setSortField,
+            sortDirection,
+            setSortDirection,
+          }}
+        />
+        <Table headers={headers} rows={rows} />
+        <Pagination />
+      </div>
+    </React.Fragment>
+  );
 }
