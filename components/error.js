6c7175e0e2f19b65520e3b93de0aca5daf7563a5
Jordan Gignac <gignjo16@gmail.com>
2021-09-06 01:38:19 -0400

    Add error component

---
+++ components/error.js
@@ -0,0 +1,24 @@
+const Error = props => {
+  return (
+    <div className="alert alert-error mb-3">
+      <div className="flex-1">
+        <svg
+          fill="none"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6 mx-2 stroke-current"
+        >
+          <path
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
+          ></path>
+        </svg>
+        <label>An error occurred: {props.error}</label>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
