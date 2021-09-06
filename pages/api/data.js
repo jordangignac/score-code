65accb9c43842636d377987f69c6470df147451e
Jordan Gignac <gignjo16@gmail.com>
2021-09-06 12:39:27 -0400

    Comments and re-organization of API routes

--- pages/api/data.js
+++ pages/api/data.js
@@ -3,16 +3,23 @@ import rushingJson from '../../data/rushing.json';
 import {sortData, filterData, paginateData} from '../../tools/data';
 import {pipeData} from '../../tools/utils';
 
+/**
+ * GET /api/data: Return filtered/sorted rushing json data
+ */
 export default function handler(req, res) {
+  // Pull relevant parameters from request query
   const {page, size, search, field, direction} = req.query;
 
+  // Pipe rushing data through filtering and sort funtions
   const data = pipeData(
     filterData(search),
     sortData(field || 'Player', direction)
   )(rushingJson);
 
-  res.status(200).json({
-    results: paginateData(page, parseInt(size))(data),
-    total: data.length,
-  });
+  // Get total results from data and paginate for client
+  const total = data.length;
+  const results = paginateData(page, parseInt(size))(data);
+
+  // Return total and results to client
+  res.status(200).json({total, results});
 }
--- pages/api/download.js
+++ pages/api/download.js
@@ -3,16 +3,27 @@ import rushingJson from '../../data/rushing.json';
 import {sortData, filterData, generateCsvString} from '../../tools/data';
 import {pipeData} from '../../tools/utils';
 
+/**
+ * GET /api/download: Return csv download response for
+ * filtered/sorted rushing data
+ */
 export default function handler(req, res) {
+  // Pull relevant parameters from request query
   const {search, field, direction} = req.query;
 
+  // Pipe rushing data through filtering and sort funtions
   const data = pipeData(
     filterData(search),
     sortData(field || 'Player', direction)
   )(rushingJson);
 
+  // Set content headers required for csv download response
   res.setHeader('Content-Disposition', 'attachment; filename=output.csv');
   res.setHeader('Content-Type', 'text/csv');
 
-  res.status(200).send(generateCsvString(data));
+  // Generate csv string from filtered/sorted rushing data
+  const result = generateCsvString(data);
+
+  // Return processed data to client
+  res.status(200).send(result);
 }
