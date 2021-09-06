8105c58f11aeedce5c4c985b54320924fde10fdf
Jordan Gignac <gignjo16@gmail.com>
2021-09-05 23:58:09 -0400

    Fix page update edge cases

--- components/pagination.js
+++ components/pagination.js
@@ -2,8 +2,8 @@ const ROW_VALUES = [25, 50, 100];
 
 const Pagination = props => {
   const {page, setPage, totalRows, rowCount, setRowCount} = props;
-  const rowTo = (page - 1) * rowCount + rowCount;
-  const rowFrom = (page - 1) * rowCount + 1;
+  const rowTo = Math.min((page - 1) * rowCount + rowCount, totalRows);
+  const rowFrom = Math.max((page - 1) * rowCount + 1, 1);
   return (
     <div className="flex align-center justify-between mt-5 px-5">
       <h3 className="mr-5">{`${rowFrom} - ${rowTo} of ${totalRows}`}</h3>
@@ -22,12 +22,14 @@ const Pagination = props => {
         </select>
         <div className="btn-group">
           <button
+            disabled={rowFrom <= 1}
             onClick={() => setPage(page - 1)}
             className="btn btn-outline btn-sm"
           >
             Previous
           </button>
           <button
+            disabled={rowTo === totalRows}
             onClick={() => setPage(page + 1)}
             className="btn btn-outline btn-sm"
           >
