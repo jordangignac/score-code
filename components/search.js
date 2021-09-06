c6fa5782090439d296496f57e86e579c921d8bb3
Jordan Gignac <gignjo16@gmail.com>
2021-09-05 23:22:52 -0400

    Update search component for sort state updates

--- components/search.js
+++ components/search.js
@@ -1,16 +1,52 @@
+const SORT_VALUES = {
+  None: '',
+  'Longest Rush': 'Lng',
+  'Total Rushing Yards': 'Yds',
+  'Total Rushing Touchdowns': 'TD',
+};
+
+const DIRECTION_VALUES = ['asc', 'desc'];
+
 const Search = props => {
-  const {searchTerm, setSearchTerm} = props;
+  const {
+    searchTerm,
+    setSearchTerm,
+    sortField,
+    setSortField,
+    sortDirection,
+    setSortDirection,
+  } = props;
   return (
-    <div className="w-full mb-3">
-      <div class="form-control">
-        <input
-          type="text"
-          placeholder="Player Search"
-          class="input input-primary input-bordered"
-          onChange={event => setSearchTerm(event.target.value)}
-          value={searchTerm}
-        />
-      </div>
+    <div className="w-full flex mb-3">
+      <input
+        type="text"
+        placeholder="Player Search"
+        className="input input-neutral input-bordered w-full mr-2"
+        onChange={e => setSearchTerm(e.target.value)}
+        value={searchTerm}
+      />
+      <select
+        value={sortField}
+        onChange={e => setSortField(e.target.value)}
+        className="select select-bordered w-50 mr-2"
+      >
+        {Object.keys(SORT_VALUES).map(key => (
+          <option key={`sort-${key}`} value={SORT_VALUES[key]}>
+            {key}
+          </option>
+        ))}
+      </select>
+      <select
+        value={sortDirection}
+        onChange={e => setSortDirection(e.target.value)}
+        className="select select-bordered w-28"
+      >
+        {DIRECTION_VALUES.map(dir => (
+          <option key={`dir-${dir}`} value={dir}>
+            {dir.toUpperCase()}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
