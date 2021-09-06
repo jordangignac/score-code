b7ee2c5ece6ab7d3cd6d52ab4fb39e2a5f578061
Jordan Gignac <gignjo16@gmail.com>
2021-09-05 22:44:52 -0400

    Fix html props for svg path

--- components/header.js
+++ components/header.js
@@ -18,9 +18,9 @@ const Header = props => {
               className="inline-block w-5 mr-2 stroke-current"
             >
               <path
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
               ></path>
             </svg>
