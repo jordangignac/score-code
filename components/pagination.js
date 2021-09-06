00345a8a2c954060df0fb60d3c313c7090b525a1
Jordan Gignac <gignjo16@gmail.com>
2021-09-05 22:56:34 -0400

    Update pagination component with count and controls

--- components/pagination.js
+++ components/pagination.js
@@ -1,20 +1,39 @@
 const Pagination = props => {
-  const {page, pages, setPage} = props;
+  const {page, setPage, totalRows, rowCount, setRowCount} = props;
+  const rowTo = (page - 1) * rowCount + rowCount;
+  const rowFrom = (page - 1) * rowCount + 1;
+  const rowCountList = [25, 50, 100];
   return (
-    <div className="flex align-center justify-center mt-5">
-      <div class="btn-group">
-        <button
-          onClick={() => setPage(page - 1)}
-          class="btn btn-outline btn-wide btn-sm"
+    <div className="flex align-center justify-between mt-5 px-5">
+      <h3 className="mr-5">{`${rowFrom} - ${rowTo} of ${totalRows}`}</h3>
+      <div className="flex align-center justify-center">
+        <select
+          value={rowCount}
+          onChange={e => setRowCount(parseInt(e.target.value))}
+          className="select select-bordered w-36 mr-5 select-sm"
         >
-          Previous Page
-        </button>
-        <button
-          onClick={() => setPage(page + 1)}
-          class="btn btn-outline btn-wide btn-sm"
-        >
-          Next Page
-        </button>
+          {rowCountList.map(count => (
+            <option
+              value={count}
+              key={`cnt-${count}`}
+              selected={count === rowCount}
+            >{`${count} per page`}</option>
+          ))}
+        </select>
+        <div className="btn-group">
+          <button
+            onClick={() => setPage(page - 1)}
+            className="btn btn-outline btn-sm"
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => setPage(page + 1)}
+            className="btn btn-outline btn-sm"
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
